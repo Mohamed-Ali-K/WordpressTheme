8b42a4028aadf95b2d031e0e1800319c9241614d
ellpatron <ell.patron91@gmail.com>
2017-11-02 11:11:57 +0100

    include jquery

--- functions.php
+++ functions.php
@@ -1,16 +1,32 @@
 <?php
+/*
+        ===========================
+        Include scripts
+        ===========================
+*/
 function brocool_script_enqueue() {
 
     wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/Brocool.css' , array(),'0.0.1' , 'all' );
     wp_enqueue_script( 'customscript', get_template_directory_uri() . '/js/brocool.js' , array(),'0.0.1', true );
+    wp_enqueue_script( 'jquery');
 }
 add_action( 'wp_enqueue_scripts', 'brocool_script_enqueue');
+/*
+        ===========================
+        Activete Menus
+        ===========================
+*/
 function brocool_theme_setup(){
     add_theme_support( 'menus' );
     register_nav_menu( 'primary', 'Primary Header Navigation' );
     register_nav_menu( 'secondery', 'secondery Header Navigation' );
 }
 add_action('init','brocool_theme_setup' );
+/*
+        ===========================
+        Activete Theme Support
+        ===========================
+*/
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
